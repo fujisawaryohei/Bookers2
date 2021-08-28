7836065e750853061f6017ab6df6fcd7c8ac83c2
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-08-28 13:42:52 +0900

    feat: 静 的ページ 正常系のRequest Specを追加

---
+++ spec/requests/homes_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe HomesController, type: :request do
+  describe 'GET /' do
+    it 'トップページを返す' do
+      get '/'
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET /homes/about' do
+    it 'Aboutページを返す' do
+      get homes_about_path
+      expect(response.status).to eq 200
+    end
+  end
+end
