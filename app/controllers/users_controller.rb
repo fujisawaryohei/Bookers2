1f00e903fb1bcd0e86e7afe1bdaee51e4bd93e0b
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-08-31 00:23:56 +0900

    feat: User情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :introduction, :email, :password, :password_confirmation)
   end
 end
