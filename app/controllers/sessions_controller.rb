2fcf9381c8fbf50358bae82d49e3765b135d8b3d
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-09-01 00:51:16 +0900

    fix: ログアウトボタンが機能しなかった問題の修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,10 +26,10 @@ class SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    user_path(resource.id)
+    user_path(resource)
   end
 
-  # TODO: 必要であれば設定する
-  # def after_sign_out_path_for(resource)
-  # end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
