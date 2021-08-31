56e4fb6105dfe225f7390306624681fe7333309e
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-09-01 00:50:13 +0900

    fix: 静的ページのアクセス権限を外した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { static_page? }
+
+  private
+
+  def static_page?
+    controller_name == 'homes'
+  end
 end
