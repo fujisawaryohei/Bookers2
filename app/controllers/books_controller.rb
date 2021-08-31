0fb005c0d2cbcec7b249e86f60ec799b00747891
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-08-31 15:56:49 +0900

    feat: i18n化と諸々修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, except: [:index, :create]
   before_action -> {
-    check_edit_permission(params[:id])
+    edit_permission_checker(params[:id])
   }, only: [:edit, :update]
 
   def index
@@ -45,9 +45,9 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-  def check_edit_permission(id)
+  def edit_permission_checker(id)
     user_id = Book.find(id).user.id
-    unless current_user == user_id
+    unless current_user.id == user_id
       redirect_to user_path(current_user), alert: "編集権限がありません"
     end
   end
