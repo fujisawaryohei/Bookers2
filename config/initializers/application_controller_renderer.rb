a41f029bb54aa7879ffe854354008e5fc2296991
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-09-03 02:07:38 +0900

    feat: RefineのSettingの修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1fd872f9682565a5e193fb9d31aaadb3d865bea167e18cebc86d6f593c3f13030430a6a473a2ee9810b023672d302924bd9c724c90fc86b9cc3b24e9ee21e94c'
