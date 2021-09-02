32bec2564437858073354e691c8eeb22270f2286
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-09-03 01:47:41 +0900

    feat: マイグレーションファイルの修正

--- db/migrate/20210828032146_add_column_to_user.rb
+++ db/migrate/20210828032146_add_column_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string, null: false
-    add_column :users, :introduction, :text, null: false
+    add_column :users, :introduction, :text
   end
 end
